631f9ff2c6353076811500f6e6a2a7c70f2159e4
Ivy K <98768550+ivykos@users.noreply.github.com>
2022-07-22 16:48:08 -0400

    Add files via upload

--- R/utils.R
+++ R/utils.R
@@ -7,41 +7,6 @@ library(dplyr)
 library(patchwork)
 library(stringr)
 
-transfer_clusters_merged <- function(seuratObj, data_set, tissue_csv){
-  #Again, make sure that the standard seurat guided clustering workflow has been followed.
-  
-  data.subset <-  subset(x=seuratObj, subset = orig.ident==data_set)
-  
-  #Retrieve the cluster labels and project ID from the Seurat object and put them in tables
-  write.table(data.subset@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
-  idents <- read.delim("tmp.tsv", header = FALSE)
-  file.remove("tmp.tsv")
-  write.table(data.subset@meta.data$orig.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
-  orig <- read.delim("tmp.tsv", header = FALSE)
-  file.remove("tmp.tsv")
-  
-  #Get the coordinates for plotting the tissue image
-  positions <- read.csv(tissue_csv, header = F)
-  
-  #Make sure only the spots within the tissue are included for this analysis
-  positions <- positions[positions$V2 == 1,]
-  
-  cells <- idents$V1
-  cells <- str_remove(cells, "_1") #Remove _1 suffix from cell names
-  idents$Cells <- cells
-  
-  pos.ordered <- positions[order(positions$V1),]
-  
-  
-  #Plot it
-  
-  plt2 <- ggplot(pos.ordered, aes(x=pos.ordered$V3, y=pos.ordered$V4, color=as.factor(idents$V2))) + 
-    geom_point() + theme_linedraw()
-  
-  plt2
-  
-}
-
 transfer_clusters <- function(obj, csv){
   positions <- data.frame(read.csv(csv, header = F))
   positions <- positions[positions$V2 == 1,]
@@ -56,33 +21,22 @@ transfer_clusters <- function(obj, csv){
   
   plt2
 }
-get_expression <- function(seuratObj, proj, feature, tissue_csv){
+
+get_expression <- function(obj, feature, csv){
   
-  #Get tissue positions again 
-  write.table(seuratObj@active.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
-  idents <- read.delim("tmp.tsv", header = FALSE)
-  file.remove("tmp.tsv")
-  write.table(seuratObj@meta.data$orig.ident, file="tmp.tsv", quote=FALSE, sep="\t", col.names = FALSE)
-  orig <- read.delim("tmp.tsv", header = FALSE)
-  file.remove("tmp.tsv")
-  
-  positions <- read.csv(tissue_csv, header = F)
+  positions <- data.frame(read.csv(csv, header = F))
   positions <- positions[positions$V2 == 1,]
+  idents <- data.frame(obj@active.ident)
+  idents <- rownames_to_column(idents)
+  pos.ordered <- positions[order(positions$V1),]
+  idents.ordered <- idents[order(idents$rowname),]
   
-  idents["Origin"] = orig$V2
-  table <- idents[idents$Origin == proj,]
-  cluster.ordered <- table[order(table$V1),]
-  pos.ordered <-positions[order(positions$V1),]
-  
-  #Extract the counts matrix from the Seurat object
-  tmp <- as.matrix(GetAssayData(object = seuratObj, slot = "counts"))
+  tmp <- as.matrix(GetAssayData(object = obj, slot = "counts"))
   feature_count <- tmp[feature,]
   rotated <- as.data.frame(t(feature_count))
   rotated <- as.data.frame(t(rotated))
-  table["Expr"] <- rotated$V1
   
   ggplot(pos.ordered, aes(pos.ordered$V3, pos.ordered$V4)) + 
-    geom_point(aes(color = table$Expr), size = 2) + 
-    scale_color_viridis(option = "inferno") + theme_bw() +ggtitle(as.character(paste(proj, feature)))
+    geom_point(aes(color = rotated$V1), size = 2) + 
+    scale_color_viridis(option = "inferno") + theme_bw()
 }
-
